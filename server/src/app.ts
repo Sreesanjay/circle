df5df4fb3ab14b8006174c010617c145ffb2e49c
Sreesanjay <sreesanjay7592@gmail.com>
2024-01-07 19:48:42 +0530

    test: added basic route for testing

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,11 @@
-import express from "express";
+import express,{Request, Response, NextFunction} from "express";
 import "dotenv/config"
 
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.get('/', (req:Request, res:Response,next:NextFunction) => {
+    console.log("request got")
+})
+
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
+
 const PORT = env.PORT;
 mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
      console.log("mongodb connected");

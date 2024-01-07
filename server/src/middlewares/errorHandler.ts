9b436693f9f1965c015a1bb58fb88f5f9b404617
Sreesanjay <sreesanjay7592@gmail.com>
2024-01-07 22:57:55 +0530

    added error handler

---
+++ server/src/middlewares/errorHandler.ts
@@ -0,0 +1,32 @@
+import { Response, Request, NextFunction } from "express";
+
+/**
+ * Error response middleware for not found
+ * @param {Request} req
+ * @param {Response} res
+ */
+export const notFound = (req: Request, res: Response) => {
+     return res.status(404).json({
+          status: "error",
+          message: "Not Found",
+     });
+};
+
+/**
+ * Generic error response middleware for validation and internal server errors
+ *
+ * @param {any} err
+ * @param {Request} req
+ * @param {Response} res
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+export const errorHandler = (err: any, req: Request, res: Response,next: NextFunction) => {
+    console.log("got")
+     const statuscode: number = res.statusCode == 200 ? 500 : res.statusCode;
+     res.status(statuscode);
+     res.json({
+          status: "error",
+          message: err?.message,
+     });
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import env from "./util/validateEnv";
 import mongoose from "mongoose";
 
 const PORT = env.PORT;
+
 mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
      console.log("mongodb connected");
      app.listen(PORT, () => console.log(`server running on port ${PORT}`));

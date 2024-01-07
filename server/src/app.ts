0f8533bb5e42e2288ce3e60ac1cb2263fa1bb811
Sreesanjay <sreesanjay7592@gmail.com>
2024-01-07 19:25:57 +0530

    complete backend setup

---
+++ server/src/app.ts
@@ -0,0 +1,5 @@
+import express from "express";
+import "dotenv/config"
+
+const app = express();
+export default app;
---
+++ server/src/server.ts
@@ -0,0 +1,3 @@
+import app from "./app";
+const PORT = process.env.PORT;
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`))

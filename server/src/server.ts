1b84c002e93533e814da8116018c6761a90a14a5
Sreesanjay <sreesanjay7592@gmail.com>
2024-02-06 16:44:54 +0530

    feat: added socket.io connection at backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,27 @@
 import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
+import * as Server from "socket.io"
 
 const PORT = env.PORT;
 
 mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
      console.log("mongodb connected");
-    const server = app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-});
+     const server = app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+     const io = new Server.Server(server, {
+          pingTimeout: 60000,
+          cors: {
+               origin: 'http://localhost:3000',
+          },
+     });
+     io.on('connection', (socket) => {
+          console.log('A user connected');
+        
+          // Handle real-time events here
+          // Example: socket.on('chat message', (message) => { ... });
+        
+          socket.on('disconnect', () => {
+            console.log('A user disconnected');
+          });
+        });
+})

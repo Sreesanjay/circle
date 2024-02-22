44bbade36b9651828b2833eabec6856bba757d36
Sreesanjay <sreesanjay7592@gmail.com>
2024-02-23 01:28:14 +0530

    added stripe secret

---
+++ client/src/components/Payment/Payment.tsx
@@ -0,0 +1,24 @@
+import { Elements, PaymentElement } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+     "pk_test_51BTUDGJAJfZb9HEBwDg86TN1KNprHjkfipXmEDMb0gSCassK5T3ZfxsAbcgKVmAIXF7oZ6ItlZZbXO6idTHE67IM007EwQ4uN3"
+);
+
+export default function App() {
+     const options = {
+          // passing the client secret obtained from the server
+          clientSecret: "{{CLIENT_SECRET}}",
+     };
+
+     return (
+          <Elements stripe={stripePromise} options={options}>
+               <form>
+                    <PaymentElement />
+                    <button>Submit</button>
+               </form>
+          </Elements>
+     );
+}
